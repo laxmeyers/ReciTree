fe03f12d996f2587a1cc99f2c533bb3bb219116f
ThomF <seniorswanky@icloud.com>
2023-04-07 11:25:13 -0600

    recipe page

--- ReciTree.client/src/pages/RecipePage.vue
+++ ReciTree.client/src/pages/RecipePage.vue
@@ -1,15 +1,34 @@
 <template>
     <div class="component">
 
-
+        {{ recipe?.name }}
     </div>
 </template>
 
 
 <script>
+import { onMounted, computed } from 'vue';
+import { useRoute } from 'vue-router';
+import { recipesService } from '../services/RecipesService';
+import { AppState } from '../AppState';
+
 export default {
     setup() {
-        return {}
+        const route = useRoute();
+        async function getThisRecipe() {
+            try {
+                const recipeId = route.params.recipeId
+                await recipesService.getThisRecipe(recipeId)
+            } catch (error) {
+                logger.error(error.message)
+            }
+        }
+        onMounted(() => {
+            getThisRecipe()
+        })
+        return {
+            recipe: computed(() => AppState.recipe)
+        }
     }
 }
 </script>

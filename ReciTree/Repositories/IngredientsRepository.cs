33a1236c7f428729b5ec0839cbfd04ce4b57acf0
Andrew <laxmeyers@gmail.com>
2023-04-07 12:06:11 -0600

    ingredients repo where

--- ReciTree/Repositories/IngredientsRepository.cs
+++ ReciTree/Repositories/IngredientsRepository.cs
@@ -4,14 +4,14 @@ namespace ReciTree.Repositories
     {
         private readonly IDbConnection _db;
 
-    public IngredientsRepository(IDbConnection db)
-    {
-        _db = db;
-    }
+        public IngredientsRepository(IDbConnection db)
+        {
+            _db = db;
+        }
 
-    internal Ingredient CreateIngredientForRecipe(Ingredient ingredientData)
-    {
-        string sql = @"
+        internal Ingredient CreateIngredientForRecipe(Ingredient ingredientData)
+        {
+            string sql = @"
         INSERT INTO
         ingredients
         (name, measurement, quantity, recipeId)
@@ -20,63 +20,63 @@ namespace ReciTree.Repositories
         SELECT LAST_INSERT_ID();
         ";
 
-        int id = _db.ExecuteScalar<int>(sql, ingredientData);
-        ingredientData.Id = id;
-        return ingredientData;
-    }
+            int id = _db.ExecuteScalar<int>(sql, ingredientData);
+            ingredientData.Id = id;
+            return ingredientData;
+        }
 
-    internal int DeleteIngredientFromRecipe(int id)
-    {
-        string sql = @"
+        internal int DeleteIngredientFromRecipe(int id)
+        {
+            string sql = @"
         DELETE FROM
         ingredients
         WHERE id = @id;
         ";
 
-        int rows = _db.Execute(sql, new{id});
-        return rows;
-    }
+            int rows = _db.Execute(sql, new { id });
+            return rows;
+        }
 
-    internal Ingredient GetIngredientById(int id)
-    {
-        string sql = @"
+        internal Ingredient GetIngredientById(int id)
+        {
+            string sql = @"
         SELECT
         * FROM ingredients
         WHERE id  = @id;
         ";
 
-        Ingredient ingredient = _db.Query<Ingredient>(sql, new{id}).FirstOrDefault();
-        return ingredient;
-    }
+            Ingredient ingredient = _db.Query<Ingredient>(sql, new { id }).FirstOrDefault();
+            return ingredient;
+        }
 
-    internal List<Ingredient> GetIngredientsForRecipe(int recipeId)
-    {
-        string sql = @"
+        internal List<Ingredient> GetIngredientsForRecipe(int recipeId)
+        {
+            string sql = @"
         SELECT
         *
         FROM ingredients
         WHERE recipeId = @recipeId;
         ";
 
-        List<Ingredient> ingredients = _db.Query<Ingredient>(sql, new{recipeId}).ToList();
-        return ingredients;
-    }
+            List<Ingredient> ingredients = _db.Query<Ingredient>(sql, new { recipeId }).ToList();
+            return ingredients;
+        }
 
-    internal int UpdateIngredient(Ingredient original)
-    {
-        string sql = @"
+        internal int UpdateIngredient(Ingredient original)
+        {
+            string sql = @"
         UPDATE
         ingredients
         SET
         measurement = @measurement,
-        quantity = @quantity,
+        quantity = @quantity
         WHERE
         id = @id;
 
         ";
 
-        int rows = _db.Execute(sql, original);
-        return rows;
-    }
+            int rows = _db.Execute(sql, original);
+            return rows;
+        }
     }
 }
